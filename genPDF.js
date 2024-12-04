8c0895130573f5481fb9afc363a01c749d8d2f81
tegaologe <101271396+tegaologe@users.noreply.github.com>
2024-12-04 12:49:57 -0700

    Changes: - Modify to expose  base64 for different project

--- genPDF.js
+++ genPDF.js
@@ -450,4 +450,9 @@ const runPdfGenerator = async (layout, data,  type = 'buffer',  res = null) => {
 };
 
 // Export the PDF generator function for use in other modules
-module.exports = runPdfGenerator;
+module.exports = {
+    runPdfGenerator,
+    pdfBase64: async (layout, data) => {
+        return await runPdfGenerator(layout, data, 'buffer');
+    }
+};
